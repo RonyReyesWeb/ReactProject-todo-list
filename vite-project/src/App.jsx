892db5808685db636d28f30d97c6ba5ee43d6642
Rony Reyes <rony@caidci.com>
2026-04-25 16:20:50 -0700

    finishing just before it breaks into its components

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -19,6 +19,24 @@ export default function App() {
 
   }
 
+function ToggleTodo(id, completed) {
+  setTodos(currentTodos => {
+    return currentTodos.map(todo => {
+      if (todo.id === id) {
+        return {...todo, completed}
+      }
+
+      return todo
+    })
+  })
+}
+
+function deleteTodo(id) {
+  setTodos(currentTodos => {
+    return currentTodos.filter(todo => todo.id !== id)
+  })
+}
+
   return (
     <>
       <form onSubmit={handlerSubmit} className="new-item-form">
@@ -35,14 +53,18 @@ export default function App() {
       </form>
       <h1 className="header">To do List</h1>
       <ul className="list">
+        {todos.length === 0 && "no Todos"}
         {todos.map((todo) => {
           return (
             <li key={todo.id}>
               <label>
-                <input type="checkbox" checked={todo.completed} />
+                <input type="checkbox" checked={todo.completed} 
+                onChange={e => ToggleTodo(todo.id, e.target.checked)} 
+                />
                 {todo.title}
               </label>
-              <button className="btn btn-danger">Delete</button>
+              <button onClick={() => deleteTodo(todo.id)} 
+              className="btn btn-danger">Delete</button>
             </li>
           )
         })}
